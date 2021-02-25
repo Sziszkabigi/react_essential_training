283df622a691b955e0bc1eecb806af7c851a9173
Brigitta Szikszai <sziszka0001@gmail.com>
2021-02-25 17:12:41 +0000

    adding list, and adding key to the list items

--- src/App.js
+++ src/App.js
@@ -9,25 +9,41 @@ function Header(props) {
 
   )
 }
+
 function Main(props) {
   return (
     <section>
       <p>This is where you can find the most {props.adjective} food around!</p>
+      <ul style={{ textAlign: "left" }}>
+      {props.dishes.map((dish) => 
+        <li key={dish.id}>{dish.title} </li>)}
+      </ul>
     </section>
   )
 }
+
 function Footer(props){
-  return(
+  return( 
     <footer>
       <h3>Copyright {props.year}</h3>
-    </footer>
+    </footer> 
   )
 }
+
+const dishes = [
+  "Mac & Cheese",
+  "Salmon",
+  "Tofu with Vegies", 
+  "Pizza"
+];
+
+const dishObjects = dishes.map( (dish, i) => ({id : i, title: dish}))
+console.log(dishObjects);
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
      <Header name="Brigi"/>
-     <Main adjective="delicious"/>
+     <Main adjective="delicious" dishes={dishObjects}/>
      <Footer year={new Date().getFullYear()}/>
     </div>
   );
