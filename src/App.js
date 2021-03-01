5681d0e94755829d90ad00f4e98fd1fd17802090
Brigitta Szikszai <sziszka0001@gmail.com>
2021-03-01 17:04:00 +0000

    Handling loading states

--- src/App.js
+++ src/App.js
@@ -56,29 +56,55 @@ function Checkbox() {
 
 function Fetch({login}) {
   const [ data, setData] = useState(null);
+  const [ loading, setLoading] = useState(null);
+  const [ error, setError] = useState(null);
 
   useEffect(() => {
+    if (!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
       .then((response) => response.json())
-      .then(setData);
-  }, []);
- 
-    if (data) {
-      //return <div>{JSON.stringify(data)}</div>
-      //Displaying data from an API...
-    return(
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+    if (loading) return <h1>Loading...</h1>;
+    if (error) 
+       return <pre>{JSON.stringify(error, null, 2)}</pre>;
+    if (!data) return null;
+
+    return (
       <div>
         <h2>Fetching data from Github </h2>
         <h1>{data.name}</h1>
         <p>{data.location}</p>
         <img alt={data.login} src={data.avatar_url}/>
-      </div>);
+      </div>
+    );
+  }
+
+    //if (data) {
+      //return <div>{JSON.stringify(data)}</div>
+      //Displaying data from an API...
+    /**return(
+     * useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+     * 
+      <div>
+        <h2>Fetching data from Github </h2>
+        <h1>{data.name}</h1>
+        <p>{data.location}</p>
+        <img alt={data.login} src={data.avatar_url}/>
+      </div>);//
     }
     return <div>
       <h2>Fetching data from Github </h2>
       <p> No User available data</p>
-      </div>
-};
+      </div>*/
+
  
 function App() {
   return (
