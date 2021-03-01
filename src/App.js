8205bd53eb862cdeca39c1668a73bbdb94910cb2
Brigitta Szikszai <sziszka0001@gmail.com>
2021-03-01 19:07:44 +0000

    adding nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useReducer } from 'react';
 import './App.css';
 import React, {useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import {Home, About, Events, Contact, Whoops404} from './pages';
 
-   
 function Header(props) {
   return (
     <header>
@@ -63,7 +64,7 @@ function Fetch({login}) {
     if (!login) return;
     setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
+      .then((response)  => response.json())
       .then(setData)
       .then(() => setLoading(false))
       .catch(setError);
@@ -114,8 +115,22 @@ function App() {
      <Footer year={new Date().getFullYear()}/>
      <Checkbox/>
      <Fetch login="Sziszkabigi"/>
+     <div>  
+      <Routes>
+        <Route path="/" element={<Home />}
+         />
+        <Route path="about" element={<About />} />
+        <Route path="events" element={<Events />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Whoops404 />} />
+      </Routes>
+    
     </div>
   );
-}
+    </div>
+  ); 
+} 
+
+
 
 export default App;
--- src/pages.js
+++ src/pages.js
@@ -1,23 +1,34 @@
-import React from 'React'
+import React from 'react';
+import {Link}  from 'react-router-dom';
 
 export function Home(){
     return(
-        <div>
-            <h1>[Company website]</h1>
-        </div>
+        <h1>[Mit tom en]</h1>
     );
 }
 export function About(){
     return(
         <div>
             <h1>[About]</h1>
+            <nav>
+                <Link to="about">About</Link>
+                <Link to="event">Events</Link>
+                <Link to="contact">Contact</Link>
+
+            </nav>
         </div>
     );
-}
+} 
 export function Events(){
     return(
         <div>
             <h1>[Events]</h1>
+            <nav>
+                <Link to="about">About</Link>
+                <Link to="event">Events</Link>
+                <Link to="contact">Contact</Link>
+
+            </nav>
         </div>
     );
 }
@@ -25,6 +36,17 @@ export function Contact(){
     return(
         <div>
             <h1>[Contact]</h1>
+            <nav>
+                <Link to="about">About</Link>
+                <Link to="event">Events</Link>
+                <Link to="contact">Contact</Link>
+
+            </nav>
         </div>
     );
+}
+export function Whoops404(){
+    <div>
+        This page is not exist
+    </div>
 }
