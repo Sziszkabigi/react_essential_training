ef5b6db8f496ad61907a9de6535396e398405ef6
Brigitta Szikszai <sziszka0001@gmail.com>
2021-03-01 18:15:15 +0000

    Configuring the router

---
+++ src/pages.js
@@ -0,0 +1,30 @@
+import React from 'React'
+
+export function Home(){
+    return(
+        <div>
+            <h1>[Company website]</h1>
+        </div>
+    );
+}
+export function About(){
+    return(
+        <div>
+            <h1>[About]</h1>
+        </div>
+    );
+}
+export function Events(){
+    return(
+        <div>
+            <h1>[Events]</h1>
+        </div>
+    );
+}
+export function Contact(){
+    return(
+        <div>
+            <h1>[Contact]</h1>
+        </div>
+    );
+}
